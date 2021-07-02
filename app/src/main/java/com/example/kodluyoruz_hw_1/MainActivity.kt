ad26164c596311528142e69b2d85b3f9bba3fa2e
Furkan Kanber <kanberfurkan@gmail.com>
2021-07-02 20:39:52 +0300

    1 onboarding screen, and 1 sign in screen is created.

---
+++ app/src/main/java/com/example/kodluyoruz_hw_1/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.kodluyoruz_hw_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+
+// ONBOARDING SCREEN 6
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun textLoginClick(view: View) {}
+}
---
+++ app/src/main/java/com/example/kodluyoruz_hw_1/SignInActivity.kt
@@ -0,0 +1,14 @@
+package com.example.kodluyoruz_hw_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+
+    fun textSignUpClick(view: View) {}
+}
